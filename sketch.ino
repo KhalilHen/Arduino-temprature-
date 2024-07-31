a669b723277380b5f5ec825f5ba6191c143f2875
Khalil <khalilhenane@hotmail.com>
2024-07-31 16:11:28 +0200

    Created a buzzer sound effect every min if it's too hot. And fixed that the green RGB led wouild work

--- sketch.ino
+++ sketch.ino
@@ -2,18 +2,25 @@ const float BETA = 3950;  // Beta coefficient for the thermistor
 const float R0 = 10000;   // Resistance of the thermistor at 25°C (10kΩ)
 const float T0 = 298.15;  // Reference temperature (25°C) in Kelvin
 const float VREF = 5.0;   // Reference voltage
-int buzzerPin1 = 8;
+int buzzerPin8 = 8;
+unsigned long previousMillis = 0;
+const long interval = 60000;
+int redPin = 13;
+int greenPin = 12;
+int textMonitor = A1;
 
-int ledPin = 13;
-int ledPin12 = 12;
+
 void setup() {
 
-pinMode(ledPin, OUTPUT);
-
+pinMode(redPin,  OUTPUT);
+  pinMode(buzzerPin8, OUTPUT);
+ pinMode(greenPin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+    unsigned long currentMillis = millis();
+
   int analogValue = analogRead(A0);
   float voltage = (analogValue / 1023.0) * VREF;  // Convert ADC value to voltage
 
@@ -26,24 +33,29 @@ void loop() {
   // Convert temperature from Kelvin to Celsius
   float temperatureC = temperatureK - 273.15;
 
-  Serial.print("Temperature: ");
+  Serial.print("Temperature:");
   Serial.println(temperatureC);
 
 
- if (temperatureC  >= 26) {
+ if (temperatureC  >= 26 ) {
 
 
-  digitalWrite(ledPin, HIGH);
- digitalWrite(ledPin12, LOW);
+  digitalWrite(redPin, HIGH);
+ digitalWrite(greenPin, LOW);
 
-
+ 
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    tone(buzzerPin8, 1000); 
+    delay(500);
+    noTone(buzzerPin8);
+  }
  
  }
 
   else  {
-Serial.println("De lamp moet nu uitgaan");
-    digitalWrite(ledPin, LOW);
-     digitalWrite(ledPin12, HIGH);
+    digitalWrite(redPin, LOW);
+     digitalWrite(greenPin, HIGH);
 
     
       }
